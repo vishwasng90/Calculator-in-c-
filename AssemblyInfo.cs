51bbe6606599d334ffba85dda7d27a3a48de751c
vishwasng90 <164770369+vishwasng90@users.noreply.github.com>
2024-06-10 14:24:18 +0530

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Calci")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Calci")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3b0a8aab-6257-4ed8-8332-1f80a84cca50")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Calci
+{
+    public partial class Form1 : Form
+    {
+        Double resultValue = 0;
+        String operationPerformed = "";
+        bool isOperationPerformed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // In this for all buttons we have given the same name 
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0" || isOperationPerformed)
+                textBox1.Clear();
+
+            isOperationPerformed = false;
+            Button button = (Button)sender;
+
+            if (button.Text == ".")
+            {
+                if (!textBox1.Text.Contains("."))
+                    textBox1.Text += button.Text;
+            }
+            else
+            {
+                textBox1.Text += button.Text;
+            }
+
+            // Append the clicked number to the label
+            labelCurrentOperation.Text += button.Text;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (resultValue != 0)
+            {
+                button16.PerformClick();
+                operationPerformed = button.Text;
+                labelCurrentOperation.Text = resultValue + " " + operationPerformed;
+                isOperationPerformed = true;
+            }
+            else
+            {
+                operationPerformed = button.Text;
+                resultValue = Double.Parse(textBox1.Text); // Converts the retrieved text into a double (a floating-point number).
+                labelCurrentOperation.Text += " " + operationPerformed;
+                isOperationPerformed = true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            labelCurrentOperation.Text = ""; // Clear the label as well
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            resultValue = 0;
+            labelCurrentOperation.Text = ""; // Clear the label as well
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                switch (operationPerformed)
+                {
+                    case "+":
+                        textBox1.Text = (resultValue + Double.Parse(textBox1.Text)).ToString();
+                        break;
+                    case "-":
+                        textBox1.Text = (resultValue - Double.Parse(textBox1.Text)).ToString();
+                        break;
+                    case "*":
+                        textBox1.Text = (resultValue * Double.Parse(textBox1.Text)).ToString();
+                        break;
+                    case "/":
+                        double divisor = Double.Parse(textBox1.Text);
+                        if (divisor != 0)
+                        {
+                            textBox1.Text = (resultValue / divisor).ToString();
+                        }
+                        else
+                        {
+                            textBox1.Text = "Infinity"; // Handle division by zero
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                
+                labelCurrentOperation.Text += " = ";
+                resultValue = double.Parse(textBox1.Text);
+                operationPerformed = ""; 
+            }
+            catch (FormatException)
+            {
+                textBox1.Text = "Infinity";
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = "Infinity";
+               
+            }
+        }
+
+        // Other methods...
+    }
+}
